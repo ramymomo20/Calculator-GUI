8b24baaf1ad428cea4771f7e9530ec2a2f0fde92
Ramy Mohammed <ramyamohammed@outlook.com>
2022-09-09 11:58:27 -0400

    Files

---
+++ calcLogic.py
@@ -0,0 +1,197 @@
+class Node:
+    def __init__(self, value):
+        self.value = value  
+        self.next = None 
+    
+    def __str__(self):
+        return "Node({self.value})"
+
+    __repr__ = __str__
+
+class Stack:
+    def __init__(self):
+        self.top = None
+    
+    def __str__(self):
+        temp = self.top
+        out = []
+        while temp:
+            out.append(str(temp.value))
+            temp = temp.next
+        out = '\n'.join(out)
+        return ('Top:{self.top}\nStack:\n{out}')
+
+    __repr__=__str__
+
+
+    def isEmpty(self):
+        return self.top == None # Validate whether stack is empty or not.
+
+    def __len__(self):
+        count = 0 # Count the number of elements in the stack.
+        current = self.top
+
+        while current is not None: 
+            current = current.next # Set the current node as the next node in the stack. 
+            count += 1
+        return count
+
+    def push(self,value): 
+        node = Node(value) # Adds an element on top of stack.
+        node.next = self.top
+        self.top = node # Sets the top of the stack as node value.
+
+     
+    def pop(self): 
+        if not self.isEmpty(): # Removes an element from stack. Validate isEmpty. 
+            temp = self.top.value # Store value temporarily to be returned.
+            self.top = self.top.next # Assign the pointer to the next Node.
+            return temp
+        else:
+            return None
+
+        
+    def peek(self): 
+        if not self.isEmpty(): # Returns the top element of the stack without removing it.
+            return self.top.value
+        else:
+            return None
+
+class Calculator:
+    def __init__(self):
+        self.__expr = None
+
+    @property
+    def getExpr(self):
+        return self.__expr
+
+    def setExpr(self, new_expr):
+        if isinstance(new_expr, str):
+            self.__expr = new_expr
+        else:
+            print('setExpr error: Invalid expression')
+            return None
+
+    def _isNumber(self, txt):
+        try:
+            float(txt)
+            return True
+        except ValueError: # When there is a ValueError, return false.
+            return False
+
+
+    def _getPostfix(self, txt):
+        postfixStack = Stack()  # method must use postfixStack to compute the postfix expression
+
+        operators = {"+" : 1, "-" : 1, "*" : 2, "/" : 2, "^" : 3} # Using dictionary keys to prioritize order of operations, from 3 -> 1.
+        (open_parenthesis_count, closed_parenthesis_count, numbers_count, operator_count) = (0, 0, 0, 0) # Initialize the counts for operators, numbers, and open/close parenthesis.
+
+        postfix = '' # postfix is going to output as a string.
+        equation = [] # Insert the contents of txt into a list for better manipulation and as a mutable data type.
+
+        for contents in txt.split(): # Take the contents in txt and split them all into a list.
+            equation.append(contents)
+
+        length = len(equation) # Count the number of elements in the list.
+
+        for tokens in range(0, length):
+            if equation[tokens] in operators.keys(): # Counts the number of operators in the equation list.
+                operator_count += 1
+                if equation[tokens] not in operators.keys(): # Check if there are no operators in the equation.
+                    return None
+            else:
+                pass
+            
+            if self._isNumber(equation[tokens]): # Count the amount of numbers in the expression.
+                numbers_count += 1
+
+            elif equation[tokens] == '(' and equation[tokens] == ')': # Count the number of open and closed parenthesis.
+                open_parenthesis_count += 1
+                closed_parenthesis_count += 1
+            else: # Else, count them separately.
+                if equation[tokens] == '(':
+                    open_parenthesis_count += 1
+                else:
+                    if equation[tokens] == ')':
+                        closed_parenthesis_count += 1
+        
+        if (numbers_count == operator_count + 1) and (open_parenthesis_count == closed_parenthesis_count): # Validate that there are no unbalanced parenthesis or more operators than numbers.
+            for contents in txt.split():
+                if not self._isNumber(contents) and contents != "(" and contents != ")": # If not a number nor contain ( and ), then check for other operators.
+                    if postfixStack.peek() == "^" and contents == "^" and not postfixStack.isEmpty(): # If ^ is in the stack and in contents, push it to the top of the stack.
+                        postfixStack.push(contents)
+                    else:
+                        while not postfixStack.isEmpty() and postfixStack.peek() != "(" and operators[contents] <= operators[postfixStack.peek()]: # Check if the stack isn't empty and that the top isn't an open parenthesis.
+                            postfix = postfix + str(postfixStack.pop()) + " " # Create the postfix by adding the stack nodes.
+                        else:
+                            postfixStack.push(contents) # If none of the conditions are met, push contents to top of stack.
+                else:
+                    if self._isNumber(contents):
+                        postfix = postfix + str(float(contents)) + " "
+
+                    if contents == "(" and contents == ")": # If the token is a parenthesis, push that to the stack.
+                        postfixStack.push(contents)
+                        if postfixStack.peek() != "(" and not postfixStack.isEmpty():
+                            postfix = postfix + str(postfixStack.pop()) + " "
+                        else:
+                            postfixStack.pop() # Removes the top node in the stack.
+
+                    elif contents == "(" and contents != ')': # Search for any open parenthesis and count the closed ones too.
+                        postfixStack.push(contents)
+                    elif contents == ')' and contents != '(':
+                        while postfixStack.peek() != "(" and not postfixStack.isEmpty():
+                            postfix = postfix + str(postfixStack.pop()) + " "
+                        else:
+                            postfixStack.pop()
+
+        else: # If neither condition is followed, return None.
+            return None
+                        
+        while not postfixStack.isEmpty(): # Check if the stack isn't empty.
+            if postfixStack.peek() not in "(": # If the top node doesn't have a open parenthesis.
+                postfix = postfix + str(postfixStack.pop()) + " "
+            else:
+                return None
+
+        return postfix[:-1] # Remove the space at the end of the postfix.
+
+
+    @property
+    def calculate(self):
+        if not isinstance(self.__expr,str) or len(self.__expr) <= 0:
+            print("Argument error in calculate")
+            return None
+
+        calcStack = Stack()   # method must use calcStack to compute the expression
+        postfix = self._getPostfix(self.__expr) # Receives the postfix expression.
+
+        if postfix != None: # Validates postfix expression.
+            for operator in postfix.split(): # Iterate through the expression.
+                if not self._isNumber(operator): 
+                    (second_number, first_number) = (calcStack.pop(), calcStack.pop()) # Set the first number and second number equal to the calcStack.pop().
+                    if operator == '^': # Exponents
+                        calcStack.push(float(first_number) ** float(second_number))
+
+                    if operator == '*': # Multiplication
+                        calcStack.push(float(first_number) * float(second_number))
+
+                    if operator == '/': # Division
+                        if second_number != 0: # Validate any divide by 0 case.
+                            calcStack.push(float(first_number) / float(second_number))
+                        else:
+                            return None
+                    if operator == '+': # Addition
+                        calcStack.push(float(first_number) + float(second_number)) # Doesn't matter the order of addition, due to the commutative property.
+
+                    if operator == '-': # Subtraction
+                        calcStack.push(float(first_number) - float(second_number))
+                else:
+                    calcStack.push(float(operator)) # If neither operator is found in the expression, push the number onto the stack.
+        else:
+            return None
+        
+        y = calcStack.pop()
+        if y - int(y) == 0:
+            return int(y)
+        else:
+            return str(round(y,4)) # After the calculations are calculated, pop the final value from the stack and return it.
---
+++ my-calculator.py
@@ -0,0 +1,268 @@
+import tkinter as tk
+from calcLogic import *
+from tkinter import *
+import math
+
+main = tk.Tk()
+main.geometry("280x230")
+main.title("Scientific Calculator")
+
+# Entry Widgets to show calculations and output
+input = Entry(main,width=20,borderwidth=2,relief=RIDGE)
+input.grid(pady=7,row=0,sticky="w",padx=20)
+
+output = Entry(main,width=12,borderwidth=2,relief=RIDGE,state='readonly',justify='center')
+output.grid(pady=5,row=1,sticky="w",padx=42)
+
+# <====================  Button Operation code starts here.. ==============>
+def calc_expression(expression):
+    x = Calculator()
+    x.setExpr(expression)
+    res = x.calculate
+    return str(res)
+
+def result():
+        try:
+            if input.get() == "":
+                output.config(state=NORMAL)
+                output.delete(0,"end")
+                output.insert("end","")
+                output.config(state='readonly')
+            elif input.get()[0] == "0":
+                input.delete(0,"end")
+                output.config(state=NORMAL)
+                output.delete(0,"end")
+                output.insert("end","0")
+                output.config(state='readonly')
+            else:
+                res = input.get()
+                res = str(res)
+                output.config(state=NORMAL)
+                input.delete(0,"end")
+                output.delete(0,"end")
+                output.insert("end",calc_expression(res))
+                output.config(state='readonly')
+        except SyntaxError:
+            input.delete(0,"end")
+            output.config(state=NORMAL)
+            output.delete(0,"end")
+            output.insert("end","invalid input")
+            output.config(state='readonly')
+# <============ end code ================>
+class Other:
+    def factorial():
+        try:
+            if input.get() == "":
+                output.config(state=NORMAL)
+                output.delete(0,"end")
+                output.insert("end","")
+                output.config(state='readonly')
+            else:
+                inp = input.get()
+                inp = int(inp)
+                for i in range (1,inp):
+                    inp *= i
+
+                output.config(state=NORMAL)
+                input.delete(0,"end")
+                output.delete(0,"end")
+                output.insert("end",str(inp))
+                output.config(state='readonly')
+
+        except SyntaxError:
+            input.delete(0,"end")
+            output.config(state=NORMAL)
+            output.delete(0,"end")
+            output.insert("end","invalid input")
+            output.config(state='readonly')  
+
+    def sinF():
+        try:
+            if input.get() == "":
+                output.config(state=NORMAL)
+                output.delete(0,"end")
+                output.insert("end","")
+                output.config(state='readonly')
+            else:
+                inp = input.get()
+                ans = math.sin(int(inp))
+
+                if ans - int(ans) == 0:
+                    ans = int(ans)
+                else:
+                    ans = str(round(ans,4))
+
+                output.config(state=NORMAL)
+                input.delete(0,"end")
+                output.delete(0,"end")
+                output.insert("end",str(ans))
+                output.config(state='readonly')
+
+        except SyntaxError:
+            input.delete(0,"end")
+            output.config(state=NORMAL)
+            output.delete(0,"end")
+            output.insert("end","invalid input")
+            output.config(state='readonly')    
+
+    def cosF():
+        try:
+            if input.get() == "":
+                output.config(state=NORMAL)
+                output.delete(0,"end")
+                output.insert("end","")
+                output.config(state='readonly')
+            else:
+                inp = input.get()
+                ans = math.cos(int(inp))
+
+                if ans - int(ans) == 0:
+                    ans = int(ans)
+                else:
+                    ans = str(round(ans,4))
+
+                output.config(state=NORMAL)
+                input.delete(0,"end")
+                output.delete(0,"end")
+                output.insert("end",str(ans))
+                output.config(state='readonly')
+
+        except SyntaxError:
+            input.delete(0,"end")
+            output.config(state=NORMAL)
+            output.delete(0,"end")
+            output.insert("end","invalid input")
+            output.config(state='readonly') 
+
+    def tanF():
+        try:
+            if input.get() == "":
+                output.config(state=NORMAL)
+                output.delete(0,"end")
+                output.insert("end","")
+                output.config(state='readonly')
+            else:
+                inp = input.get()
+                ans = math.tan(int(inp))
+
+                if ans - int(ans) == 0:
+                    ans = int(ans)
+                else:
+                    ans = str(round(ans,4))
+
+                output.config(state=NORMAL)
+                input.delete(0,"end")
+                output.delete(0,"end")
+                output.insert("end",str(ans))
+                output.config(state='readonly')
+
+        except SyntaxError:
+            input.delete(0,"end")
+            output.config(state=NORMAL)
+            output.delete(0,"end")
+            output.insert("end","invalid input")
+            output.config(state='readonly') 
+# <============= Button Design Code starts here.. ==================>
+clear = Button(main, text="⌫",width=5,command=lambda:input.delete(len(input.get())-1,"end"),bg="blue",fg="white",relief=RIDGE)
+clear.grid(row=0,sticky="e",padx=155,pady=5)
+
+
+clearall = Button(main,text="C",width=2,command=lambda:input.delete(0,"end"),bg="red",fg="white",relief=RIDGE)
+clearall.grid(row=0,sticky="e",padx=115,pady=5)
+
+
+
+result = Button(main,text="=",width=7,command=result,bg="red",fg="white",borderwidth=3,relief=RIDGE)
+result.grid(row=1,sticky="w",padx=130)
+
+
+
+leftParenth = Button(text="(",width=2,command=lambda:input.insert("end"," ( "),borderwidth=3,relief=RIDGE)
+leftParenth.grid(row=2,sticky="w",padx=15,pady=5)
+
+rightParenth = Button(text=")",width=2,command=lambda:input.insert("end"," ) "),borderwidth=3,relief=RIDGE)
+rightParenth.grid(row=2,sticky="w",padx=45,pady=5)
+
+sqrt = Button(text="√x",width=2,command=lambda:input.insert("end"," ^ ( 1 / 2 ) "),borderwidth=3,relief=RIDGE)
+sqrt.grid(row=2,sticky="w",padx=75,pady=5)
+
+square = Button(text="^",width=2,command=lambda:input.insert("end"," ^ "),borderwidth=3,relief=RIDGE)
+square.grid(row=2,sticky="w",padx=105,pady=5)
+
+plus = Button(main,text="+",width=2,command=lambda:input.insert("end"," + "),borderwidth=3,relief=RIDGE)
+plus.grid(row=2,sticky="e",padx=155,pady=5)
+
+
+#####################
+fact = Button(text="x!",width=2,command=Other.factorial,borderwidth=3,relief=RIDGE)
+fact.grid(row=2,sticky="e",padx=125,pady=5)
+#####################
+
+
+nine = Button(text="9",width=2,command=lambda:input.insert("end","9"),borderwidth=3,relief=RIDGE)
+nine.grid(row=3,sticky="w",padx=15,pady=5)
+
+eight = Button(text="8",width=2,command=lambda:input.insert("end","8"),borderwidth=3,relief=RIDGE)
+eight.grid(row=3,sticky="w",padx=45,pady=5)
+
+seven = Button(main,text="7",width=2,command=lambda:input.insert("end","7"),borderwidth=3,relief=RIDGE)
+seven.grid(row=3,sticky="w",padx=75,pady=5)
+
+six = Button(text="6",width=2,command=lambda:input.insert("end","6"),borderwidth=3,relief=RIDGE)
+six.grid(row=3,sticky="w",padx=105,pady=5)
+
+minus = Button(main,text="-",width=2,command=lambda:input.insert("end"," - "),borderwidth=3,relief=RIDGE)
+minus.grid(row=3,sticky="e",padx=155,pady=5)
+
+
+#####################
+sine = Button(text="sin",width=2,command=Other.sinF,borderwidth=3,relief=RIDGE)
+sine.grid(row=3,sticky="e",padx=125,pady=5)
+#####################
+
+
+five = Button(text="5",width=2,command=lambda:input.insert("end","5"),borderwidth=3,relief=RIDGE)
+five.grid(row=4,sticky="w",padx=15,pady=5)
+
+four = Button(main,text="4",width=2,command=lambda:input.insert("end","4"),borderwidth=3,relief=RIDGE)
+four.grid(row=4,sticky="w",padx=45,pady=5)
+
+three = Button(text="3",width=2,command=lambda:input.insert("end","3"),borderwidth=3,relief=RIDGE)
+three.grid(row=4,sticky="w",padx=75,pady=5)
+
+two = Button(text="2",width=2,command=lambda:input.insert("end","2"),borderwidth=3,relief=RIDGE)
+two.grid(row=4,sticky="w",padx=105,pady=5)
+
+multiply = Button(main,text="*",width=2,command=lambda:input.insert("end"," * "),borderwidth=3,relief=RIDGE)
+multiply.grid(row=4,sticky="e",padx=155,pady=5)
+
+
+#####################
+cosine = Button(text="cos",width=2,command=Other.cosF,borderwidth=3,relief=RIDGE)
+cosine.grid(row=4,sticky="e",padx=125,pady=5)
+#####################
+
+
+one = Button(main,text="1",width=2,command=lambda:input.insert("end","1"),borderwidth=3,relief=RIDGE)
+one.grid(row=5,sticky="w",padx=15,pady=5)
+
+zero = Button(text="0",width=2,command=lambda:input.insert("end","0"),borderwidth=3,relief=RIDGE)
+zero.grid(row=5,sticky="w",padx=45,pady=5)
+
+double_zero = Button(text="00",width=2,command=lambda:input.insert("end","00"),borderwidth=3,relief=RIDGE)
+double_zero.grid(row=5,sticky="w",padx=75,pady=5)
+
+dot = Button(main,text=".",width=2,command=lambda:input.insert("end","."),borderwidth=3,relief=RIDGE)
+dot.grid(row=5,sticky="w",padx=105,pady=5)
+
+divide = Button(main,text="/",width=2,command=lambda:input.insert("end"," / "),borderwidth=3,relief=RIDGE)
+divide.grid(row=5,sticky="e",padx=155,pady=5)
+
+
+#####################
+tangent = Button(text="tan",width=2,command=Other.tanF,borderwidth=3,relief=RIDGE)
+tangent.grid(row=5,sticky="e",padx=125,pady=5)
+#####################
+
+
+main.mainloop()
